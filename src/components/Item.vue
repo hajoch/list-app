50616e5f422f9f5c1bb4f03daab262ce81430e84
Hallvard Jore Christensen <hallvard.jore.christensen@gmail.com>
2020-09-25 16:47:13 +0200

    Creating List and item component

---
+++ src/components/Item.vue
@@ -0,0 +1,73 @@
+<template>
+  <transition name="fade">
+    <div class="item">
+        <input type="checkbox" name="" :id="id">
+        <label :for="id">{{ item.title }}</label>
+    </div>
+  </transition>
+</template>
+
+<script>
+let GID = 1
+export default {
+    props: {
+        item: {
+            type: Object
+        }
+    },
+    data () {
+        return {
+            id: `item-${GID++}`
+        }
+    },
+}
+</script>
+
+<style lang="scss">
+.item {
+  display: flex;
+  padding: 12px 0;
+  border-bottom: 1px solid #eee;
+  font-size: 14px;
+}
+.item input {
+  display: none;
+}
+.item label {
+  flex: 1;
+  line-height: 20px;
+}
+.item label:before,
+.item label:after {
+  content: '';
+  display: inline-block;
+  margin-right: 20px;
+  margin-top: 1px;
+  width: 14px;
+  height: 14px;
+  vertical-align: top;
+}
+.item label:before {
+  border: 1px solid #ccc;
+  border-radius: 2px;
+  background-color: white;
+}
+.item label:after {
+  content: '\f00c';
+  position: relative;
+  display: none;
+  z-index: 10;
+  margin-right: -16px;
+  width: 10px;
+  height: 10px;
+  padding: 3px;
+  border-radius: 2px;
+  font: normal normal normal 10px/1 FontAwesome;
+  color: white;
+  background-color: #ccc;
+  float: left;
+}
+.item input:checked + label:after {
+  display: inline-block;
+}
+</style>
---
+++ src/components/List.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+      <ul>
+          <li
+            v-for="item in items"
+            :key="item.id"
+          >
+                <item
+                    :item="item"
+                />
+          </li>
+      </ul>
+  </div>
+</template>
+
+<script>
+import Item from './Item.vue'
+
+export default {
+    components: {
+        Item
+    },
+    data () {
+        return {
+            items: [
+                { title: 'Potteplante'},
+                { title: 'Rensemiddel til kaffemaskinen'},
+                { title: 'Bakepapir'},
+            ]
+        }
+    },
+}
+</script>
+
+<style scoped lang="scss">
+    item-list {
+        padding: 0 5rem;
+        height: 400px; //temporary
+        transition: all 0.5 ease;
+        list-style-type: none;
+
+        > ul,
+        > ul > li {
+            display: flex;
+            height: 100%;
+        }
+        > ul > li {
+            flex: 1;
+            transition: all 0.5 ease;
+        }
+    }
+</style>
